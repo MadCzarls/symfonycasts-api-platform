742650257c569525c9ac9e61714187962f4f4dd5
Karol Kasiarz <k.kasiarz@andersenlab.com>
2021-09-20 11:57:32 +0200

    chapter 12

--- app/src/Entity/CheeseListing.php
+++ app/src/Entity/CheeseListing.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\BooleanFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 use App\Repository\CheeseListingRepository;
 use Carbon\Carbon;
 use DateTimeImmutable;
@@ -27,6 +30,8 @@ use function nl2br;
     denormalizationContext: ['groups' => ['cheese_listing:write'], 'swagger_definition_name' => 'Write'],
     normalizationContext: ['groups' => ['cheese_listing:read'], 'swagger_definition_name' => 'Read'],
 )]
+#[ApiFilter(BooleanFilter::class, properties: ['isPublished'])]
+#[ApiFilter(SearchFilter::class, properties: ['title' => 'partial'])]
 class CheeseListing
 {
     /**
